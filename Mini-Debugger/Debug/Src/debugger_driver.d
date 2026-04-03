5d760292aa6e4b15c28a05315852a27df7751c65
ARJUN KRISHNA M <76147257+ARJUNMANIPUZHA@users.noreply.github.com>
2026-04-03 08:48:34 +0530

    Delete Mini-Debugger directory

--- Mini-Debugger/Debug/Src/debugger_driver.d
+++
@@ -1,8 +0,0 @@
-Src/debugger_driver.o: ../Src/debugger_driver.c ../Inc/stm32f407disc1.h \
- ../Inc/gpio_driver.h ../Inc/uart_driver.h ../Inc/rcc_driver.h \
- ../Inc/debugger_driver.h
-../Inc/stm32f407disc1.h:
-../Inc/gpio_driver.h:
-../Inc/uart_driver.h:
-../Inc/rcc_driver.h:
-../Inc/debugger_driver.h:
